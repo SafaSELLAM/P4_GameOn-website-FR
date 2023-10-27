7fb559aaa84e60c17951df59f00c8f0fe824e4cf
SafaSELLAM <safa.sellam@outlook.fr>
2023-10-27 11:14:49 +0200

    Co-authored-by: Kévin Monteiro <Keirodev@users.noreply.github.com>

--- starterOnly/JS/validate.js
+++ starterOnly/JS/validate.js
@@ -15,15 +15,6 @@ function validateField(field, checkIfValidate) {
   checkIfValidate()
     ? field.parentElement.removeAttribute("data-error-visible")
     : field.parentElement.setAttribute("data-error-visible", "true");
-
-  /*
-  try {
-    validateFunction();
-    field.parentElement.removeAttribute("data-error-visible");
-  } catch (error) {
-    field.parentElement.setAttribute("data-error-visible", "true");
-  }
-  */
 }
 
 //function that checks inputs values
@@ -34,39 +25,59 @@ function validate(event) {
   const firstname = document.getElementById("first");
   const lastname = document.getElementById("last");
   const email = document.getElementById("email");
+  const birthdate = document.getElementById("birthdate");
   const integer = document.getElementById("quantity");
   const termsCheckbox = document.getElementById("checkbox1");
   const location = document.getElementsByName("location");
 
-  // firstname and lastname must be >= 2
+  // firstname and lastname must be >= 2, return false if not
 
   validateField(firstname, () => {
-    /*
-   if (firstname.value.length < 2) return false;
-   return true
-  */
     return !(firstname.value.length < 2);
   });
+
   validateField(lastname, () => {
-    if (lastname.value.length < 2) {
-      console.log("==>", error);
-      return error;
-    }
+    return !(lastname.value.length < 2);
   });
   //checks email pattern
 
   validateField(email, () => {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zAZ0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(email.value)) {
-      return error;
-    }
+    return !!emailPattern.test(email.value);
   });
+
+  //controlbirthdate min age =18 and max =65
+  validateField(birthdate, () => {
+    const minAge = 18;
+    const maxAge = 65;
+    const birthdateInput = document.getElementById("birthdate");
+    const currentDate = new Date();
+    const maxDate = new Date(
+      currentDate.getFullYear() - minAge,
+      currentDate.getMonth(),
+      currentDate.getDate()
+    );
+    const minDate = new Date(
+      currentDate.getFullYear() - maxAge,
+      currentDate.getMonth(),
+      currentDate.getDate()
+    );
+
+    // Convert date to  format ISO (aaaa-mm-jj)
+    const maxDateISO = maxDate.toISOString().split("T")[0];
+    const minDateISO = minDate.toISOString().split("T")[0];
+
+    // Define date min and max
+    birthdateInput.setAttribute("max", maxDateISO);
+    birthdateInput.setAttribute("min", minDateISO);
+
+    return !!birthdate.validity.valid;
+  });
+
   //checks if it is a number
 
   validateField(integer, () => {
-    if (integer.value.trim() === "" || isNaN(integer.value)) {
-      return error;
-    }
+    return !(integer.value.trim() === "" || isNaN(integer.value));
   });
   //checks all inputs with the name location to see if at least one has been checked
   let radioSelected = false;
@@ -77,15 +88,11 @@ function validate(event) {
     }
   }
   validateField(location[0], () => {
-    if (!radioSelected) {
-      return error;
-    }
+    return !!radioSelected;
   });
   //checks that the terms and condition has been checked
   validateField(termsCheckbox, () => {
-    if (!termsCheckbox.checked) {
-      return error;
-    }
+    return !!termsCheckbox.checked;
   });
   // if no errors, display the validation message
   if (!document.querySelector(".formData[data-error-visible=true]")) {
